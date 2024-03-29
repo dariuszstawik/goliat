14db05bc02a6223247a5e2aa4ce9e88523a6821d
Dariusz Stawik <dariusz.stawik@gmail.com>
2024-03-29 19:54:41 +0100

    Update navbar asset

--- src/app/[locale]/components/global-components/navbar-asset/index.js
+++ src/app/[locale]/components/global-components/navbar-asset/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function NavbarAsset() {
   return (
     <Image
-      src="/navbar-asset.png"
+      src="/asset.svg"
       width={303}
       height={61}
       className="hidden lg:block"
