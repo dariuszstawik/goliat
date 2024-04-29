484a26a7d132415ab7f925535310fc1cefa5a23b
Dariusz Stawik <dariusz.stawik@gmail.com>
2024-04-29 23:37:03 +0200

    Fix errors

--- src/app/[locale]/components/global-components/contact-form/index.js
+++ src/app/[locale]/components/global-components/contact-form/index.js
@@ -31,16 +31,10 @@ const ContactForm = ({
     e.preventDefault();
 
     emailjs.sendForm(
-      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
-        ? process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
-        : "",
-      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID
-        ? process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID
-        : " ",
+      process.env.EMAILJS_SERVICE_ID ? process.env.EMAILJS_SERVICE_ID : "",
+      process.env.EMAILJS_TEMPLATE_ID ? process.env.EMAILJS_TEMPLATE_ID : " ",
       form.current ? form.current : "",
-      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-        ? process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
-        : ""
+      process.env.EMAILJS_PUBLIC_KEY ? process.env.EMAILJS_PUBLIC_KEY : ""
     );
 
     setShowNotification(true);
